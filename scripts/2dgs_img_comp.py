3dad7f0d637e75f6661843a07f7c9e8ebd621eaf
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-07-17 23:00:03 +0800

    Update 2dgs_img_comp.py

--- scripts/2dgs_img_comp.py
+++ scripts/2dgs_img_comp.py
@@ -1,5 +1,6 @@
-import random
+import os
 import math
+import subprocess as sp
 
 import numpy as np
 import torch
@@ -9,7 +10,6 @@ import torchvision.transforms.functional as trnsF
 import lightning.pytorch as pl
 import matplotlib.pyplot as plt
 
-from torchao.float8.float8_utils import amax_to_scale, tensor_to_scale, to_fp8_saturated
 from pytorch_msssim import ms_ssim, ssim
 from PIL import Image
 from tqdm import trange
@@ -20,35 +20,13 @@ from anyschedule import AnySchedule
 from igs.gs2d import GaussianSplatting2D
 
 
-torch.set_float32_matmul_precision("medium")
+torch.set_float32_matmul_precision("high")
 DEVICE = "cuda:0"
 EPS = 1e-6
 
 
-def quant(x, target_dtype=torch.float8_e4m3fn):
-    if target_dtype in [torch.int8]:
-        max_value = torch.iinfo(target_dtype).max
-    else:
-        max_value = torch.finfo(target_dtype).max
-    if x.ndim == 3:
-        amax = torch.max(torch.abs(x), dim=1, keepdim=True)[0].to(torch.float64)
-        scale = max_value / torch.clamp(amax, min=1e-12)
-    else:
-        amax = torch.max(torch.abs(x)).to(torch.float64)
-        scale = max_value / torch.clamp(amax, min=1e-12)
-    x_scaled = x.to(scale) * scale
-    x_quant = x_scaled.clamp(-max_value, max_value).to(target_dtype)
-    return x_quant, scale
-
-
-def dequant(x, scale):
-    x = x.to(torch.float64)
-    scale = scale.to(torch.float64)
-    return (x / scale).float()
-
-
 class ProgressiveGaussianSplatter(nn.Module):
-    def __init__(self, num_gaussians, image=None):
+    def __init__(self, num_gaussians, image=None, use_offset=True):
         super().__init__()
         self.num_gaussians = num_gaussians
 
@@ -102,13 +80,14 @@ class ProgressiveGaussianSplatter(nn.Module):
                 dim=1,
             )
         else:
-            raw_colors = torch.rand(1, num_gaussians, 3)
+            raw_colors = torch.zeros(1, num_gaussians, 3)
 
         # Initialize Gaussian parameters
+        self.use_offset = use_offset
         self.position = nn.Parameter(position_grid[None])
         self.log_scale = nn.Parameter(torch.zeros(1, num_gaussians, 2) - 5)
         self.rotation = nn.Parameter(torch.zeros(1, num_gaussians))
-        self.color = nn.Parameter(raw_colors + 1)
+        self.color = nn.Parameter(raw_colors + int(use_offset))
         self.register_buffer("alphas", torch.ones(1, num_gaussians))
 
     def forward(self, num_active_features=None, size=None):
@@ -119,18 +98,15 @@ class ProgressiveGaussianSplatter(nn.Module):
         x_grid, y_grid = GaussianSplatting2D.xy_grid(size, self.position.device)
         x_grid = x_grid[None]
         y_grid = y_grid[None]
-        return (
-            GaussianSplatting2D.render(
-                self.position[:, :num_active_features],
-                self.color[:, :num_active_features],
-                torch.exp(self.log_scale[:, :num_active_features]) + EPS,
-                self.rotation[:, :num_active_features],
-                self.alphas[:, :num_active_features],
-                x_grid,
-                y_grid,
-            )
-            - 1
-        )
+        return GaussianSplatting2D.render(
+            self.position[:, :num_active_features],
+            self.color[:, :num_active_features],
+            torch.exp(self.log_scale[:, :num_active_features]) + EPS,
+            self.rotation[:, :num_active_features],
+            self.alphas[:, :num_active_features],
+            x_grid,
+            y_grid,
+        ) - int(self.use_offset)
 
 
 lpips = LPIPS(net="alex").to(DEVICE).requires_grad_(False).eval()
@@ -139,39 +115,35 @@ lpips2 = LPIPS(net="vgg").to(DEVICE).requires_grad_(False).eval()
 
 def gaussian_splatting_loss(rendered_image, target_image):
     """Combined L1 + L2 loss"""
-    l1 = F.l1_loss(rendered_image, target_image)
     l2 = F.mse_loss(rendered_image, target_image)
-    msssim = 1 - ms_ssim(
-        rendered_image, target_image, data_range=1.0, win_size=7, size_average=True
-    )
     ssim_loss = 1 - ssim(
-        rendered_image, target_image, data_range=1.0,size_average=True
+        rendered_image, target_image, data_range=1.0, size_average=True
     )
-    lpips_loss1 = lpips(rendered_image, target_image, normalize=True)
-    lpips_loss2 = lpips2(rendered_image, target_image, normalize=True)
-    return l1 + l2 * 0.5 + ssim_loss * 0.5 + msssim * 0.3 + lpips_loss1 * 0.1 + lpips_loss2 * 0.1
+    msssim = 1 - ms_ssim(
+        rendered_image, target_image, data_range=1.0, size_average=True
+    )
+    return ssim_loss + msssim * 0.25 + l2 * 0.25
 
 
 def psnr(image1, image2):
-    return 20 * torch.log10(1.0 / torch.mean((image1 - image2) ** 2))
+    return 10 * torch.log10(1.0 / torch.mean((image1 - image2) ** 2))
 
 
-def progressive_training():
+def progressive_training(run_name, file):
     # Parameters
-    # 256px -> 512px -> 1024px, 800/400/200 step
+    # 256px -> 512px -> 1024px, 1600/600/300 step
     height, width = 256, 256
     scales = [1, 2, 4]
-    steps = [1400, 400, 200]
-    # scales = [1]
-    # steps = [1000]
+    steps = [1600, 600, 300]
     max_height, max_width = max(scales) * height, max(scales) * width
-    num_gaussians = 8192
+    # 1bpp for 1024px img in 10bit config
+    num_gaussians = 2048
     log_period = 20
-    lr = 0.05 * 4096 / num_gaussians
+    lr = min(0.05 * 4096 / num_gaussians, 0.03)
 
     # Create target image
     # target_image = create_target_image(height, width)
-    image = Image.open("./data/target-real-sq.jpg").convert("RGB")
+    image = Image.open(file).convert("RGB")
     target_image_base = (
         trnsF.to_tensor(
             trnsF.center_crop(
@@ -189,7 +161,12 @@ def progressive_training():
     )
 
     # Initialize splatter
-    splatter = ProgressiveGaussianSplatter(num_gaussians, target_image_base).to(DEVICE)
+    splatter = ProgressiveGaussianSplatter(
+        num_gaussians,
+        target_image_base,
+        # None,
+        use_offset=False,
+    ).to(DEVICE)
     optimizer = torch.optim.AdamW(
         splatter.parameters(),
         lr=lr,
@@ -204,7 +181,7 @@ def progressive_training():
             "lr": {
                 "mode": "cosine",
                 "end": sum(steps) + 1,
-                "min_value": 0.1,
+                "min_value": 0.05,
                 "warmup": 0,
             }
         },
@@ -227,18 +204,12 @@ def progressive_training():
                 rendered_image,
                 target_image,
             )
-            # recon_loss = ssim(
-            #     rendered_image,
-            #     target_image,
-            #     data_range=1.0,
-            #     size_average=True,
-            # )
 
             ## regularization
             position_penalty = torch.mean(F.relu(splatter.position.abs() - 1.0))
             reg_loss = position_penalty
 
-            loss = recon_loss - reg_loss * 0.01
+            loss = recon_loss + reg_loss * 0.01
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
@@ -250,67 +221,60 @@ def progressive_training():
             )
             if s % log_period == 0:
                 coarse_to_fine_visualization(
-                    splatter, target_image, file_name=f"log_images2/{s//log_period}"
+                    splatter,
+                    target_image,
+                    file_name=f"logs/{run_name}/prog/{s//log_period}",
                 )
         prev_step += step
 
     coarse_to_fine_visualization(
         splatter,
         target_image_base,
-        file_name=f"log_images2/{(prev_step//log_period) + int(prev_step%log_period > 0)}",
+        file_name=f"logs/{run_name}/prog/{(prev_step//log_period) + int(prev_step%log_period > 0)}",
     )
 
     return splatter, target_image_base, losses
 
 
 @torch.no_grad()
-def coarse_to_fine_visualization(model, target_image, file_name=None):
+def coarse_to_fine_visualization(model, target_image, file_name=None, run_name=None):
     """
     Visualize coarse-to-fine AR generation
     """
+    assert (
+        file_name is not None or run_name is not None
+    ), "file_name or run_name must be provided"
     # clone splatter to avoid affect training
-    splatter = ProgressiveGaussianSplatter(model.num_gaussians).to(DEVICE).eval()
+    splatter = (
+        ProgressiveGaussianSplatter(model.num_gaussians, use_offset=model.use_offset)
+        .to(DEVICE)
+        .eval()
+    )
     splatter.load_state_dict(model.state_dict())
-    fractions = [1 / 16, 1 / 8, 1 / 4, 1 / 2, 1.0]
-    num_gaussians = splatter.num_gaussians
     height, width = target_image.shape[2:]
-
-    # Calculate number of Gaussians for each fraction
-    features_counts = [max(1, int(frac * num_gaussians)) for frac in fractions]
 
     # Generate images for each level
     with torch.no_grad():
-        rendered_images = []
-        for count in features_counts:
-            rendered = splatter(num_active_features=count, size=(height, width)).cpu()
-            rendered_images.append(rendered)
+        rendered = splatter(size=(height, width)).cpu()
 
     # Create visualization
-    fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+    fig, axes = plt.subplots(1,2, figsize=(10, 4))
 
     # Target image
     img = target_image[0].cpu().permute(1, 2, 0).clamp(0, 1)
     h, w = img.shape[:2]
-    axes[0, 0].imshow(img)
-    axes[0, 0].set_title(f"Target Image ({w}x{h})")
-    axes[0, 0].axis("off")
+    axes[0].imshow(img)
+    axes[0].set_title(f"Target Image ({w}x{h})")
+    axes[0].axis("off")
 
-    # Progressive generations
-    titles = ["1/16", "1/8", "1/4", "1/2", "Full"]
-    positions = [(0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
-
-    for i, (rendered, title, pos) in enumerate(zip(rendered_images, titles, positions)):
-        row, col = pos
-        axes[row, col].imshow(rendered[0].permute(1, 2, 0).clamp(0, 1))
-        axes[row, col].set_title(f"{title}\n(First {features_counts[i]} Gaussians)")
-        axes[row, col].axis("off")
+    axes[1].axis("off")
+    axes[1].imshow(rendered[0].permute(1, 2, 0).clamp(0, 1))
+    axes[1].set_title(f"Rendered Image")
 
     plt.tight_layout()
-    file_name = file_name or "output/coarse_to_fine"
+    file_name = file_name or f"logs/{run_name}/caorse_to_fine"
     plt.savefig(f"{file_name}.png", dpi=200)
     plt.close()
-
-    return rendered_images, features_counts
 
 
 @torch.no_grad()
@@ -320,36 +284,22 @@ def save_tensor_image(tensor, file_name, **kwargs):
     img.save(file_name, **kwargs)
 
 
-def main():
-    pl.seed_everything(0)
-    """Main training and visualization"""
-    print("=== Progressive Gaussian Splatting Training ===")
-
-    # Run progressive training
-    splatter, target_image, losses = progressive_training()
-
-    eval(splatter, target_image)
-
-    torch.save(splatter.state_dict(), "output/features.pt")
-
-
 def sim_to_db(sim_val):
-    return 10 * math.log10(1/(1-sim_val))
+    return 10 * math.log10(1 / (1 - sim_val))
 
 
 @torch.no_grad()
-def eval(splatter: ProgressiveGaussianSplatter, target_image: torch.Tensor):
-    save_tensor_image(target_image, "output/target.png")
+def eval(
+    splatter: ProgressiveGaussianSplatter, target_image: torch.Tensor, run_name: str
+):
+    save_tensor_image(target_image, f"logs/{run_name}/target.png")
     print("\n=== Statistics ===")
     b, c, h, w = target_image.shape
     pixel_count = h * w
     gaussian_per_pixel = splatter.num_gaussians / pixel_count
     ppp = gaussian_per_pixel * 8
-    bpp = ppp * 8
-    print(f"Gaussians per pixel: {gaussian_per_pixel:.2f}")
-    print(f"Parameters per pixel: {ppp:.2f}")
-
-    root_state_dict = {k: v.clone().cpu() for k, v in splatter.state_dict().items()}
+    print(f"Gaussians per pixel: {gaussian_per_pixel:.4f}")
+    print(f"Parameters per pixel: {ppp:.4f}")
 
     print("\n=== Evaluation ===")
     recon = splatter(size=(h, w)).clamp(0, 1)
@@ -365,92 +315,45 @@ def eval(splatter: ProgressiveGaussianSplatter, target_image: torch.Tensor):
     print(f"MS-SSIM(db): {sim_to_db(msssim_val):.2f}")
     print(f"LPIPS (AlexNet): {lpips_alex:.4f}")
     print(f"LPIPS (VGG): {lpips_vgg:.4f}")
-    save_tensor_image(recon, "output/recon.png")
+    save_tensor_image(recon, f"logs/{run_name}/recon.png")
 
-    print("\n=== Evaluation (16bit) ===")
-    splatter.load_state_dict(
-        {k: dequant(*quant(v, torch.float16)) for k, v in root_state_dict.items()}
+
+def main():
+    pl.seed_everything(0)
+    run_name = "2dgs_img_comp-2048-sashimi-ssim-msssim-l2-2.5kstep-nooffset"
+    file = "./data/sashimi.jpg"
+
+    os.makedirs(f"logs/{run_name}", exist_ok=True)
+    os.makedirs(f"logs/{run_name}/prog", exist_ok=True)
+
+    """Main training and visualization"""
+    print("=== Progressive Gaussian Splatting Training ===")
+
+    # Run progressive training
+    splatter, target_image, losses = progressive_training(run_name, file)
+    torch.save(splatter.state_dict(), f"logs/{run_name}/features.pt")
+
+    eval(splatter, target_image, run_name)
+
+    sp.run(
+        [
+            "ffmpeg",
+            "-r",
+            "10",
+            "-start_number",
+            "0",
+            "-y",
+            "-i",
+            f"logs/{run_name}/prog/%d.png",
+            "-c:v",
+            "libx264",
+            "-pix_fmt",
+            "yuv420p",
+            f"logs/{run_name}/prog.mp4",
+        ],
+        capture_output=True,
+        check=True,
     )
-    b, c, h, w = target_image.shape
-    recon = splatter(size=(h, w)).clamp(0, 1)
-    psnr_val = psnr(recon, target_image).item()
-    ssim_val = ssim(recon, target_image, data_range=1, size_average=True).item()
-    msssim_val = ms_ssim(recon, target_image, data_range=1, size_average=True).item()
-    lpips_alex = 1 - lpips(recon, target_image, normalize=True).item()
-    lpips_vgg = 1 - lpips2(recon, target_image, normalize=True).item()
-    print(f"PSNR: {psnr_val:5.2f}")
-    print(f"SSIM: {ssim_val:.4f}")
-    print(f"SSIM(db): {sim_to_db(ssim_val):.2f}")
-    print(f"MS-SSIM: {msssim_val:.4f}")
-    print(f"MS-SSIM(db): {sim_to_db(msssim_val):.2f}")
-    print(f"LPIPS (AlexNet): {lpips_alex:.4f}")
-    print(f"LPIPS (VGG): {lpips_vgg:.4f}")
-    save_tensor_image(recon, "output/recon_fp16.png")
-
-    print("\n=== Evaluation (12bit) ===")
-    state_dict = {
-        k: dequant(*quant(v, torch.int8)) for k, v in splatter.state_dict().items()
-    }
-    state_dict["position"] = root_state_dict["position"]
-    state_dict["log_scale"] = root_state_dict["log_scale"]
-    splatter.load_state_dict(state_dict)
-    b, c, h, w = target_image.shape
-    recon = splatter(size=(h, w)).clamp(0, 1)
-    psnr_val = psnr(recon, target_image).item()
-    ssim_val = ssim(recon, target_image, data_range=1, size_average=True).item()
-    msssim_val = ms_ssim(recon, target_image, data_range=1, size_average=True).item()
-    lpips_alex = 1 - lpips(recon, target_image, normalize=True).item()
-    lpips_vgg = 1 - lpips2(recon, target_image, normalize=True).item()
-    print(f"PSNR: {psnr_val:5.2f}")
-    print(f"SSIM: {ssim_val:.4f}")
-    print(f"SSIM(db): {sim_to_db(ssim_val):.2f}")
-    print(f"MS-SSIM: {msssim_val:.4f}")
-    print(f"MS-SSIM(db): {sim_to_db(msssim_val):.2f}")
-    print(f"LPIPS (AlexNet): {lpips_alex:.4f}")
-    print(f"LPIPS (VGG): {lpips_vgg:.4f}")
-    save_tensor_image(recon, "output/recon_12bit.png")
-
-    print("\n=== Evaluation (10bit) ===")
-    state_dict = {
-        k: dequant(*quant(v, torch.int8)) for k, v in splatter.state_dict().items()
-    }
-    state_dict["position"] = root_state_dict["position"]
-    splatter.load_state_dict(state_dict)
-    b, c, h, w = target_image.shape
-    recon = splatter(size=(h, w)).clamp(0, 1)
-    psnr_val = psnr(recon, target_image).item()
-    ssim_val = ssim(recon, target_image, data_range=1, size_average=True).item()
-    msssim_val = ms_ssim(recon, target_image, data_range=1, size_average=True).item()
-    lpips_alex = 1 - lpips(recon, target_image, normalize=True).item()
-    lpips_vgg = 1 - lpips2(recon, target_image, normalize=True).item()
-    print(f"PSNR: {psnr_val:5.2f}")
-    print(f"SSIM: {ssim_val:.4f}")
-    print(f"SSIM(db): {sim_to_db(ssim_val):.2f}")
-    print(f"MS-SSIM: {msssim_val:.4f}")
-    print(f"MS-SSIM(db): {sim_to_db(msssim_val):.2f}")
-    print(f"LPIPS (AlexNet): {lpips_alex:.4f}")
-    print(f"LPIPS (VGG): {lpips_vgg:.4f}")
-    save_tensor_image(recon, "output/recon_10bit.png")
-
-    print("\n=== Evaluation (int8) ===")
-    splatter.load_state_dict(
-        {k: dequant(*quant(v, torch.int8)) for k, v in root_state_dict.items()}
-    )
-    b, c, h, w = target_image.shape
-    recon = splatter(size=(h, w)).clamp(0, 1)
-    psnr_val = psnr(recon, target_image).item()
-    ssim_val = ssim(recon, target_image, data_range=1, size_average=True).item()
-    msssim_val = ms_ssim(recon, target_image, data_range=1, size_average=True).item()
-    lpips_alex = 1 - lpips(recon, target_image, normalize=True).item()
-    lpips_vgg = 1 - lpips2(recon, target_image, normalize=True).item()
-    print(f"PSNR: {psnr_val:5.2f}")
-    print(f"SSIM: {ssim_val:.4f}")
-    print(f"SSIM(db): {sim_to_db(ssim_val):.2f}")
-    print(f"MS-SSIM: {msssim_val:.4f}")
-    print(f"MS-SSIM(db): {sim_to_db(msssim_val):.2f}")
-    print(f"LPIPS (AlexNet): {lpips_alex:.4f}")
-    print(f"LPIPS (VGG): {lpips_vgg:.4f}")
-    save_tensor_image(recon, "output/recon_int8.png")
 
 
 if __name__ == "__main__":
